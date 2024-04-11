6673a59982e3049d4efdc9693557e13de0cfe1e1
Abner <58574013+Abnersampf@users.noreply.github.com>
2024-04-11 17:04:10 -0300

    Update 2529. Maximum Count of Positive Integer and Negative Integer.cs

--- 2529. Maximum Count of Positive Integer and Negative Integer.cs
+++ 2529. Maximum Count of Positive Integer and Negative Integer.cs
@@ -1,13 +1,14 @@
-static int MaximumCount(int[] nums)
-{
-    int pos = 0, neg = 0;
-    foreach (int n in nums)
-        if (n != 0)
-        {
-            if (n < 0)
-                neg++;
-            else
-                pos++;
-        }
-    return Math.Max(pos, neg);
+public class Solution {
+    public int MaximumCount(int[] nums) {
+        int pos = 0, neg = 0;
+        foreach (int n in nums)
+            if (n != 0)
+            {
+                if (n < 0)
+                    neg++;
+                else
+                    pos++;
+            }
+        return Math.Max(pos, neg);
+    }
 }
