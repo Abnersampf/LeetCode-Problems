62323106fd8884126a830f8f42552bb86ba65f0f
Abner <58574013+Abnersampf@users.noreply.github.com>
2024-04-11 18:23:32 -0300

    Update 1. Two Sum.cs

--- 1. Two Sum.cs
+++ 1. Two Sum.cs
@@ -1,10 +1,13 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
         for (int i = 0; i < nums.Length; i++)
-            for (int j = i; j < nums.Length; j++)
-                if (j != i)
-                    if (nums[i] + nums[j] == target)
-                        return new int[2] { i, j };
-        return null;
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                    return [i, j];
+            }
+        }
+        return [0, 1];
     }
 }
